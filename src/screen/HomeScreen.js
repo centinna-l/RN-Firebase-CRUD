0052ee44ca44742ace61bf7ef9ca6bdacba67ba1
centinna-l <jerryjoy99@gmail.com>
2020-06-22 13:23:52 +0530

    Fetching Data

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,9 +1,70 @@
-import React from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  ActivityIndicator,
+} from "react-native";
 import { firebase } from "../firebase/FirebaseConfig";
 
 const HomeScreen = (props) => {
-  console.log(props);
+  const [entityText, setEntityText] = useState("");
+  const [entities, setEntities] = useState();
+  //const [loading, setLoading] = useState(true);
+
+  const entityRef = firebase.firestore().collection("entities");
+  const userEmail = props.extraData.email;
+  //console.log(props);
+
+  useEffect(() => {
+    entityRef.onSnapshot(
+      (querySnapshot) => {
+        const newEntities = [];
+        querySnapshot.forEach((doc) => {
+          const entity = doc.data();
+          entity.id = doc.id;
+          newEntities.push(entity);
+          console.log(entity);
+          //setEntities(entities.push(data))
+        });
+        setEntities(newEntities);
+        //console.log(entities)
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    
+    console.log(entities)
+    //alert(entities);
+
+    return () => entityRef;
+  }, []);
+
+  const onAddButtonPress = () => {
+    if (entityText && entityText.length > 0) {
+      const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+      const data = {
+        text: entityText,
+        email: userEmail,
+        createdAt: timestamp,
+      };
+      entityRef
+        .add(data)
+        .then((_doc) => {
+          setEntityText("");
+          Keyboard.dismiss();
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  };
   const onSignoutPress = async () => {
     try {
       await firebase.auth().signOut();
@@ -12,12 +73,51 @@ const HomeScreen = (props) => {
       alert(e);
     }
   };
+
+  const renderEntity = ({ item, index }) => {
+    return (
+      <View style={styles.entityContainer}>
+        <Text style={styles.entityText}>
+          {index}. {item.text}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <TouchableOpacity style={styles.button} onPress={() => onSignoutPress()}>
-        <Text style={styles.buttonTitle}>Sign Out</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.formContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add new entity"
+            placeholderTextColor="#aaaaaa"
+            onChangeText={(text) => setEntityText(text)}
+            value={entityText}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
+          />
+          <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
+            <Text style={styles.buttonText}>Add</Text>
+          </TouchableOpacity>
+        </View>
+        {entities && (
+          <View style={styles.listContainer}>
+            <FlatList
+              data={entities}
+              renderItem={renderEntity}
+              keyExtractor={(item) => item.id}
+              removeClippedSubviews={true}
+            />
+          </View>
+        )}
+        {/* <View>
+        <TouchableOpacity style={styles.button} onPress={onSignoutPress}>
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </View> */}
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -26,32 +126,53 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
   },
+  formContainer: {
+    flexDirection: "row",
+    height: 80,
+    marginTop: 40,
+    marginBottom: 20,
+    flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   input: {
     height: 48,
     borderRadius: 5,
     overflow: "hidden",
     backgroundColor: "white",
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 30,
-    marginRight: 30,
     paddingLeft: 16,
+    flex: 1,
+    marginRight: 5,
   },
-
   button: {
-    backgroundColor: "#788eec",
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 20,
-    height: 48,
+    height: 47,
     borderRadius: 5,
+    backgroundColor: "#788eec",
+    width: 80,
     alignItems: "center",
     justifyContent: "center",
   },
-  buttonTitle: {
+  buttonText: {
     color: "white",
     fontSize: 16,
-    fontWeight: "bold",
+  },
+  listContainer: {
+    marginTop: 20,
+    padding: 20,
+  },
+  entityContainer: {
+    marginTop: 16,
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+    paddingBottom: 16,
+  },
+  entityText: {
+    fontSize: 20,
+    color: "#333333",
   },
 });
 
